9f5f214630ad5be87682f5bb6eef1a91a717b6c3
den4ikr <55842394+den4ikr@users.noreply.github.com>
2021-01-17 13:08:54 +0200

    Update api.ts
    Added axios.create function

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,10 +1,16 @@
 import axios from "axios"
 
+const APIKEY = "fed7d1a56fd2de2f78944ed8d70c4f97"
+
+const instance = axios.create ({
+    baseURL: "https://api.openweathermap.org/data/2.5/",
+})
+
 export const API = {
     getWeather (q: string) {
-        return axios.get (`https://api.openweathermap.org/data/2.5/weather?q=${q}&appid=fed7d1a56fd2de2f78944ed8d70c4f97`)
+        return instance.get (`weather?q=${q}&appid=${APIKEY}`)
     },
     getWeatherByLatAndLon (lat: number, lon: number) {
-        return axios.get (`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=fed7d1a56fd2de2f78944ed8d70c4f97`)
+        return instance.get (`weather?lat=${lat}&lon=${lon}&appid=${APIKEY}`)
     }
 }
